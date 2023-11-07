e2bd6d2730c67a5c440c650222f6b90811e114ec
AricapaPinto <aricapaalberth@gmail.com>
2023-11-06 20:09:53 -0500

    Adding the first module with tables and input record.

---
+++ ApiPhp-AgroGestor/insumos/ObtenerInsumos.php
@@ -0,0 +1,13 @@
+<?php 
+	header("Access-Control-Allow-Origin: * "); // Permite el acceso desde cualquier origen, o usa "http://localhost" si solo quieres permitirlo desde localhost.
+	header("Access-Control-Allow-Methods: GET, POST");
+	header("Access-Control-Allow-Headers: Content-Type");
+
+    include_once("../usuarios/Conexion.php");
+
+    $consulta = $base_de_datos->query("SELECT * FROM insumos ORDER BY fecha_compra DESC");
+
+    $datos = $consulta->fetchAll();
+
+    $respuesta['datos'] = $datos;
+    echo json_encode($respuesta);
