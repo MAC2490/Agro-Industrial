1976e12f921ccfc9dd0931ffb9879b48594e2196
Mauricio <mauricioaguirre2490@gmail.com>
2023-11-12 15:49:43 -0500

    Error

--- GestorAgroIndustrial/src/clases/ButtonEditor.java
+++ GestorAgroIndustrial/src/clases/ButtonEditor.java
@@ -8,29 +8,29 @@ import javax.swing.JCheckBox;
 import javax.swing.JTable;
 
 public class ButtonEditor extends DefaultCellEditor {
-        private Object clickedValue;
+    private Object clickedValue;
 
-        public ButtonEditor(JCheckBox checkBox) {
-            super(checkBox);
-            checkBox.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    fireEditingStopped();
-                }
-            });
-        }
-
-        @Override
-        public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
-            if (value instanceof Component) {
-                clickedValue = value;
-                return (Component) value;
+    public ButtonEditor(JCheckBox checkBox) {
+        super(checkBox);
+        checkBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                fireEditingStopped();
             }
-            return super.getTableCellEditorComponent(table, value, isSelected, row, column);
-        }
-
-        @Override
-        public Object getCellEditorValue() {
-            return clickedValue;
-        }
+        });
     }
+
+    @Override
+    public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
+        if (value instanceof Component) {
+            clickedValue = value;
+            return (Component) value;
+        }
+        return super.getTableCellEditorComponent(table, value, isSelected, row, column);
+    }
+
+    @Override
+    public Object getCellEditorValue() {
+        return clickedValue;
+    }
+}
